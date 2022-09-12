3bea94fb7c2382aa218e0d78b45c7519a44b13c4
Lee.wiseman@bourne-leisure.co.uk <leew21@hotmail.co.uk>
2022-09-12 21:19:21 +0100

    fix: container with background color

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -7,21 +7,23 @@ export interface ContainerProps {
   children?: React.ReactNode;
   className?: string;
   maxWidth?: string;
+  backgroundColor?: string;
 }
 
 const Container = ({
   children,
   className,
   maxWidth = "1000px",
+  backgroundColor = "white",
 }: ContainerProps) => {
   const containerClassNames = cx(styles.container, className);
 
   return (
-    <>
+    <div className={styles.fullWidthContainer} style={{ backgroundColor }}>
       <div className={containerClassNames} style={{ maxWidth }}>
         {children}
       </div>
-    </>
+    </div>
   );
 };
 
